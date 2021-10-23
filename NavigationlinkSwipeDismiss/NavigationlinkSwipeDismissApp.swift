2236a358920f022ec89a73ee18c2da011028e7b5
Yajima Yohei <yajima.y.yohei@gmail.com>
2021-10-23 16:46:41 +0900

    Initial Commit

---
+++ NavigationlinkSwipeDismiss/NavigationlinkSwipeDismissApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationlinkSwipeDismissApp.swift
+//  NavigationlinkSwipeDismiss
+//
+//  Created by Yajima Youhei on 2021/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationlinkSwipeDismissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
